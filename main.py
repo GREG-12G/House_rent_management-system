4a196dd1928b487e43fbff5488b9798b29791ed0
GREG <gregmaina618@gmail.com>
2026-09-08 13:14:33 +0300

    Implement menu choice handling in main.py
    Added user input handling for menu choices.

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ print("3. Record House Payment")
 print("4. View House")
 print("5. View Tenant")
 print("6. Exit")
+
+choice = input("enter your choice;")
+
+if choice == "1":
+    print("Add House selected")
+elif choice == "2":
+    print("Add Tenants selected")
+elif choice == "3":
+    print("Record Rent Payment selected")
+elif choice == "4":
+    print("View Houses selected")
+elif choice == "5":
+    print("View Tenants selected")
+elif choice == "6":
+    print("Goodbye")
+else
+    print("Invalid Choice")
