3410567e9b7262cede95ea65cfadbd6639998afe
GREG <gregmaina618@gmail.com>
2026-09-08 13:21:00 +0300

    Update print statements for singular tenant and house

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ choice = input("enter your choice;")
 if choice == "1":
     print("Add House selected")
 elif choice == "2":
-    print("Add Tenants selected")
+    print("Add Tenant selected")
 elif choice == "3":
     print("Record Rent Payment selected")
 elif choice == "4":
-    print("View Houses selected")
+    print("View House selected")
 elif choice == "5":
-    print("View Tenants selected")
+    print("View Tenant selected")
 elif choice == "6":
     print("Goodbye")
 else
